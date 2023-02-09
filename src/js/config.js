78d7b95e4a06c5fe5fdf656067a4033ac7eb997f
Marcello C <marcello_ct@hotmail.com>
2023-02-09 00:52:06 -0300

    forkify-v1

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// into this file, we will basically put all variables that should be constants and should be reused across the project
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '088b92cb-d75d-4ce8-b936-1c3f65523575';
+export const MODAL_CLOSE_SEC = 2.5;
